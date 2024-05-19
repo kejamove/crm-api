ed228d04bc79856c53b48f45db52e758d685e386
muasya-victor <vicmwe184@gmail.com>
2024-05-19 15:49:53 +0300

    updated cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,9 +21,10 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\CorsMiddleware::class,
     ];
 
-    
+
 
     /**
      * The application's route middleware groups.
@@ -44,7 +45,6 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class, 
         ],
     ];
 
