e85b5ea95b5b5c1e47d8d0bb1de3ea4ea23b0935
victor muasya <vicmwe184@gmail.com>
2024-08-06 13:59:34 +0300

    allow cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://kejacrm.netlify.app/', 'http://localhost:5174', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
