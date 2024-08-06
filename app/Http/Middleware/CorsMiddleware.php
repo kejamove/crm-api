c786fd11f72dc103a13b24cd905fdff6c0268e71
victor muasya <vicmwe184@gmail.com>
2024-08-06 20:42:19 +0300

    allow headers in CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://kejacrm.netlify.app');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
