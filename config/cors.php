67a4df6d55bdcbb344f808f8eda7b088cda87574
victor muasya <vicmwe184@gmail.com>
2024-08-06 15:03:11 +0300

    allow headers

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://kejacrm.netlify.app', 'http://localhost:5174', 'http://localhost:5173'],
+    'allowed_origins' => ['https://kejacrm.netlify.app', 'http://localhost:5174', 'http://localhost:5173'],
 
-    'allowed_origins_patterns' => ["*https://kejacrm.netlify.app*"],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
